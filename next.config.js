d62241bda4ae223e19635c67de0c5b8b3d431137
Kate Holterhoff <kateholterhoff@Kates-MacBook-Pro-2.local>
2025-06-17 13:04:19 -0400

    refactor for security

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
-    domains: [
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
-    ],
+    unoptimized: false,
     remotePatterns: [
       {
         protocol: "https",
@@ -17,16 +11,6 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
         pathname: "/**",
       },
     ],
